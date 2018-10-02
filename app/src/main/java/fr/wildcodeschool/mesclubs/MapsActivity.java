15823fc0311c689dba2311581ecab6884234c0d7
Romain Marca <rmarca@hotmail.fr>
2018-10-02 17:34:18 +0200

    test geoloc avorté

--- app/src/main/java/fr/wildcodeschool/mesclubs/MapsActivity.java
+++ app/src/main/java/fr/wildcodeschool/mesclubs/MapsActivity.java
@@ -1,8 +1,18 @@
 package fr.wildcodeschool.mesclubs;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -22,6 +32,96 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        checkPermission();
+    }
+
+    LocationManager mLocationManager = null;
+
+    @SuppressLint("MissingPermission")
+    public void initLocation(){
+        mLocationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+        LocationListener locationListener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+               // effectuer une action ici !
+                moveCameraOnUser(location);
+            }
+
+            public void onStatusChanged(String provider, int status, Bundle extras) {}
+
+            public void onProviderEnabled(String provider) {}
+
+            public void onProviderDisabled(String provider) {}
+        };
+
+        mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+        mLocationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+    }
+
+    private void checkPermission() {
+
+        // vérification de l'autorisation d'accéder à la position GPS
+
+        if (ContextCompat.checkSelfPermission(MapsActivity.this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // l'autorisation n'est pas acceptée
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(MapsActivity.this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                // l'autorisation a été refusée précédemment, on peut prévenir l'utilisateur ici
+
+            } else {
+
+                // l'autorisation n'a jamais été réclamée, on la demande à l'utilisateur
+
+                ActivityCompat.requestPermissions(MapsActivity.this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        100);
+            }
+        } else {
+
+            initLocation();
+            // TODO : autorisation déjà acceptée, on peut faire une action ici
+
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 100: {
+
+                // cas de notre demande d'autorisation
+
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // TODO : l'autorisation a été donnée, nous pouvons agir
+                    initLocation();
+
+                } else {
+
+                    // l'autorisation a été refusée :(
+
+                }
+                return;
+            }
+        }
+    }
+
+
+    public void moveCameraOnUser(Location location){
+
+        LatLng userLocation = new LatLng(location.getLatitude(), location.getLongitude());
+        mMap.addMarker(new MarkerOptions().position(userLocation).title("You"));
+        //mMap.setMyLocationEnabled(true);
+        //CameraUpdate yourLocation = CameraUpdateFactory.newLatLngZoom(userLocation, 18);
+        //mMap.moveCamera(yourLocation);
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(userLocation, 17.0f));
     }
 
 
@@ -38,9 +138,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
     }
 }
