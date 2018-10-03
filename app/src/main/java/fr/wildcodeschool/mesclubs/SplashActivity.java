5cf75748d26174b61af9938ce327b2e4801c6432
Pierre Fajardo <pierrefaja@gmail.com>
2018-10-03 15:20:40 +0200

    correction code splash

--- app/src/main/java/fr/wildcodeschool/mesclubs/SplashActivity.java
+++ app/src/main/java/fr/wildcodeschool/mesclubs/SplashActivity.java
@@ -11,13 +11,15 @@ import android.widget.ImageView;
 public class SplashActivity extends AppCompatActivity {
     private static int SPLASH_TIME_OUT = 3000;
 
-    ImageView run;
-    Animation fade;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        ImageView run;
+        Animation fade;
 
         run = (ImageView) findViewById(R.id.runningMan);
         fade = AnimationUtils.loadAnimation(this,R.anim.fade);
