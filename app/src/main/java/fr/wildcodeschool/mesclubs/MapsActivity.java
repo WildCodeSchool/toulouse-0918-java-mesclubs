1f463423fe553386f026ead94f2bc5d51ff538bf
Ariane <arianeh.ah@gmail.com>
2018-10-05 12:17:51 +0200

    espace inutiles suppriné :P

--- app/src/main/java/fr/wildcodeschool/mesclubs/MapsActivity.java
+++ app/src/main/java/fr/wildcodeschool/mesclubs/MapsActivity.java
@@ -36,15 +36,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     boolean moveCam = false;
     private GoogleMap mMap;
     private DrawerLayout mDrawerLayout;
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-
-
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
